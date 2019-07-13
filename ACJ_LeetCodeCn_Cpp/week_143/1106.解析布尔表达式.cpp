4ad7686e9c616c9bd8f1a106ae26aae10587a8c0
IACJ <iacjacj@qq.com>
2019-07-13 17:54:39 +0800

    finish week_143

--- ACJ_LeetCodeCn_Cpp/week_143/1106.解析布尔表达式.cpp
+++ ACJ_LeetCodeCn_Cpp/week_143/1106.解析布尔表达式.cpp
@@ -8,134 +8,146 @@ using namespace std;
 class Solution
 {
 public:
-     bool parseBoolExpr(string expression)
-     {
-          if (expression.size() == 1)
-          {
-               if (expression[0] =='t')
-               {
-                    return true;
-               }
-               else
-               {
-                    return false;
-               }
-          }
+    bool parseBoolExpr(string expression)
+    {
+        stack_char_.clear();
 
-          vector<char> stack_op;
-          vector<bool> stack_val;
+        for (int i = 0; i < expression.size(); i++)
+        {
+            char c = expression[i];
 
-          int i = 0;
+            switch (c)
+            {
+            case '!':
+                stack_char_.push_back(c);
+                break;
+            case '&':
+                stack_char_.push_back(c);
+                break;
+            case '|':
+                stack_char_.push_back(c);
+                break;
+            case '(': 
+                stack_char_.push_back(c);
+                break;
+            case ')':
+                this->closeBrackets();
+                break;
+            case ',':
+                stack_char_.push_back(c);
+                break;
+            case 't':
+                this->pushValue(true);
+                break;
+            case 'f':
+                this->pushValue(false);
+                break;
+            default:
+                assert(false);
+                break;
+            }
+            // for (auto i : stack_char_)
+            // {
+            //     cout << i;
+            // }
+            // cout << endl;
+        }
 
-          while (i < expression.size())
-          {
-               char c = expression[i];
-               switch (c)
-               {
-               case '!':
-               {
-                    stack_op.push_back('!');
-                    i++;
-                    break;
-               }
-               case '|':
-               {
-                    stack_op.push_back('|');
-                    i++;
-                    break;
-               }
-               case '&':
-               {
-                    stack_op.push_back('&');
-                    i++;
-                    break;
-               }
-               case 't':
-               {
-                    bool this_val = true;
-                    if (stack_val.empty())
-                    {
-                         stack_val.push_back(this_val);
-                    }
-                    else
-                    {
-                         bool last_val = stack_val.back();
-                         stack_val.pop_back();
-                         if (stack_op.back() == '|')
-                         {
-                              this_val = last_val || this_val;
-                         }
-                         else if (stack_op.back() == '&')
-                         {
-                              this_val = last_val && this_val;
-                         }
-                         stack_val.push_back(this_val);
-                    }
-                    break;
-               }
-               case 'f':
-               {
-                    bool this_val = false;
-                    if (stack_val.empty())
-                    {
-                         stack_val.push_back(this_val);
-                    }
-                    else
-                    {
-                         bool last_val = stack_val.back();
-                         stack_val.pop_back();
-                         if (stack_op.back() == '|')
-                         {
-                              this_val = last_val || this_val;
-                         }
-                         else if (stack_op.back() == '&')
-                         {
-                              this_val = last_val && this_val;
-                         }
-                         stack_val.push_back(this_val);
-                    }
-                    break;
-               }
-               case ')':
-               {
-                    if (stack_op.back() == '!')
-                    {
-                         bool this_val = stack_val.back();
-                         stack_val.pop_back();
-                         stack_val.push_back(!this_val);
-                    }
-                    stack_op.pop_back();
-                    break;
-               }
-               case ',':
-               {  
-                    break;
-               }
+        assert((stack_char_.size() == 1));
 
-               default:
-               {
-                    break;
-               }
-               }
-               i++;
-          }
-
-          return stack_val[0];
+        if (stack_char_[0] == 't')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
      }
+
+    void pushValue(bool this_value){
+
+        if (stack_char_.empty() || stack_char_.back() != ',')
+        {
+            if (this_value)
+            {
+                stack_char_.push_back('t');
+            }
+            else
+            {
+                stack_char_.push_back('f');
+            }
+        }
+        else
+        {
+            char last_value_char = stack_char_[stack_char_.size() - 2];
+            bool last_value = false;
+            if (last_value_char == 't')
+            {
+                last_value = true;
+            }
+            else
+            {
+                last_value = false;
+            }
+            char last_op_char = stack_char_[stack_char_.size() - 4];
+            bool final_value = false;
+            if (last_op_char == '|')
+            {
+                final_value = last_value || this_value;
+            }
+            else if (last_op_char == '&')
+            {
+                final_value = last_value && this_value;
+            }
+            stack_char_.pop_back(); // ,
+            stack_char_.pop_back(); // t/f
+            this->pushValue(final_value);
+        }
+    }
+
+    void closeBrackets(){
+        if (stack_char_.size() >= 3)
+        {
+            char last_value_char = stack_char_[stack_char_.size() - 1];
+            bool last_value = false;
+            if (last_value_char == 't')
+            {
+                last_value = true;
+            }
+            else
+            {
+                last_value = false;
+            }
+            char last_op_char = stack_char_[stack_char_.size() - 3];
+            if (last_op_char == '!')
+            {
+                last_value = !last_value;
+            }
+
+            stack_char_.pop_back(); // t/f
+            stack_char_.pop_back(); // (
+            stack_char_.pop_back(); // !
+
+
+            this->pushValue(last_value);
+        }
+    }
+
+private:
+    vector<char> stack_char_;
 };
 
 int main()
 {
-    cout << endl
-         << "hello!" << endl;
-    
+    cout << "hello!" << endl;
+
     Solution solution;
 
-    solution.parseBoolExpr("!(&(&(f),&(!(t),&(f),|(f)),&(!(&(f)),&(t),|(f,f,t))))");
+    cout << solution.parseBoolExpr("!(&(&(f),&(!(t),&(f),|(f)),&(!(&(f)),&(t),|(f,f,t))))") << endl;
+    cout << solution.parseBoolExpr("!(f)") << endl;
 
     cout
-        << endl
-        << "Done." << endl
-        << endl;
+        << "Done." << endl;
     return 0;
 }
